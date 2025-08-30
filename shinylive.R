89f76120a5cf519ea4427ae20aeb6efba481efa2
bradduthie <brad.duthie@gmail.com>
2025-08-30 10:46:53 +0100

    Revise with the desolve package included

---
+++ shinylive.R
@@ -0,0 +1,20 @@
+shinylive::export("BIOU9PC_code/practical-01", "BIOU9PC", 
+                  subdir = "practical-01");
+
+shinylive::export("BIOU9PC_code/practical-02", "BIOU9PC", 
+                  subdir = "practical-02");
+
+shinylive::export("BIOU9PC_code/practical-03", "BIOU9PC", 
+                  subdir = "practical-03");
+
+shinylive::export("BIOU9PC_code/practical-04", "BIOU9PC", 
+                  subdir = "practical-04");
+
+shinylive::export("BIOU9PC_code/practical-05", "BIOU9PC", 
+                  subdir = "practical-05");
+
+shinylive::export("BIOU9PC_code/practical-06", "BIOU9PC", 
+                  subdir = "practical-06");
+
+shinylive::export("BIOU9PC_code/practical-07", "BIOU9PC", 
+                  subdir = "practical-07");
