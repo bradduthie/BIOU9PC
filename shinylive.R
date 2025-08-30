53606302daa36a6ccd59ecb33790c5caa62e2a65
bradduthie <brad.duthie@gmail.com>
2025-08-30 13:09:57 +0100

    Try to get practical 5 on shinylive by repliacing ode with a custom function

--- shinylive.R
+++ shinylive.R
@@ -1,12 +1,20 @@
+# Shiny live is the best one to use for Practical 1
+# https://bradduthie.github.io/BIOU9PC/practical-01/
 shinylive::export("BIOU9PC_code/practical-01", "BIOU9PC", 
                   subdir = "practical-01");
 
+# Shiny live is the best one to use for Practical 2
+# https://bradduthie.github.io/BIOU9PC/practical-02/
 shinylive::export("BIOU9PC_code/practical-02", "BIOU9PC", 
                   subdir = "practical-02");
 
+# Shiny live is the best one to use for Practical 3
+# https://bradduthie.github.io/BIOU9PC/practical-03/
 shinylive::export("BIOU9PC_code/practical-03", "BIOU9PC", 
                   subdir = "practical-03");
 
+# Shiny live is the best one to use for Practical 4
+# https://bradduthie.github.io/BIOU9PC/practical-04/
 shinylive::export("BIOU9PC_code/practical-04", "BIOU9PC", 
                   subdir = "practical-04");
 
