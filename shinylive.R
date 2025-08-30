9e55e8f90f9ba87f1db6e9bf15508f0d7cc55e4f
bradduthie <brad.duthie@gmail.com>
2025-08-30 13:56:41 +0100

    add the URLs -- just to summarise, what has happened is that I needed to replace anything using the desolve package by replacing the ode function with a custom version of ode that works with ShinyLive

--- shinylive.R
+++ shinylive.R
@@ -1,28 +1,27 @@
-# Shiny live is the best one to use for Practical 1
 # https://bradduthie.github.io/BIOU9PC/practical-01/
 shinylive::export("BIOU9PC_code/practical-01", "BIOU9PC", 
                   subdir = "practical-01");
 
-# Shiny live is the best one to use for Practical 2
 # https://bradduthie.github.io/BIOU9PC/practical-02/
 shinylive::export("BIOU9PC_code/practical-02", "BIOU9PC", 
                   subdir = "practical-02");
 
-# Shiny live is the best one to use for Practical 3
 # https://bradduthie.github.io/BIOU9PC/practical-03/
 shinylive::export("BIOU9PC_code/practical-03", "BIOU9PC", 
                   subdir = "practical-03");
 
-# Shiny live is the best one to use for Practical 4
 # https://bradduthie.github.io/BIOU9PC/practical-04/
 shinylive::export("BIOU9PC_code/practical-04", "BIOU9PC", 
                   subdir = "practical-04");
 
+# https://bradduthie.github.io/BIOU9PC/practical-05/
 shinylive::export("BIOU9PC_code/practical-05", "BIOU9PC", 
                   subdir = "practical-05");
 
+# https://bradduthie.github.io/BIOU9PC/practical-06/
 shinylive::export("BIOU9PC_code/practical-06", "BIOU9PC", 
                   subdir = "practical-06");
 
+# https://bradduthie.github.io/BIOU9PC/practical-07/
 shinylive::export("BIOU9PC_code/practical-07", "BIOU9PC", 
                   subdir = "practical-07");
